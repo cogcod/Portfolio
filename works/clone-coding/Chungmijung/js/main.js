638e10f816caf1d9293c62c7e339b8e3a607265d
cogcod <91929726+cogcod@users.noreply.github.com>
2022-02-16 01:15:07 +0900

    Chung_js_contents

--- works/clone-coding/Chungmijung/js/main.js
+++ works/clone-coding/Chungmijung/js/main.js
@@ -1,4 +1,4 @@
-// 슬라이드 이동
+// --- main slides ---
 $(function(){
 
     const $slidesContainer = $('.slides__img');
@@ -47,4 +47,91 @@ $(function(){
         clearInterval();
         autoPlay();
     });
+});
+
+
+// --- contents slides ---
+$(function(){
+
+    const $contentsSlides = $('.slides__product');
+    const $pagination = $('.contents__pagination>li');
+
+    let nowIdx = 0;
+    let intervalkey = '';
+
+    function moveFn(){
+        $pagination.eq(nowIdx).addClass('on').siblings().removeClass('on');
+        $contentsSlides.stop().animate({
+            left: -490 * nowIdx
+        },400);
+    }
+
+    $pagination.on('click',function(evt){
+        evt.preventDefault();
+        nowIdx = $pagination.index(this);
+        moveFn();
+    });
+
+    function nextIdx(){
+        if(nowIdx<2){
+            nowIdx++;
+        }else{
+            nowIdx=0;
+        }
+    };
+
+    function autoPlay(){
+        intervalkey = setInterval(function(){
+            nextIdx();
+            moveFn();
+        },3000);
+    }
+
+    $(window).on('load',function(){
+        clearInterval();
+        autoPlay();
+    });
+});
+
+
+// --- news slides ---
+$(function(){
+
+    const $btnPlay = $('.btn_play');
+    const $btnPause = $('.btn_pause');
+    const $newsTxt = $('.news__txt>li');
+    const $slides = $('.slides__news__img>img');
+
+    let intervalkey = '';
+
+    // 자동실행
+    function autoPlay(){
+        intervalkey = setInterval(function(){
+            $slides.fadeToggle(1000);
+            $newsTxt.fadeToggle(1000);
+        },3000);
+    }
+
+    // 정지
+    function autoPause(){
+        clearInterval(intervalkey);
+    }
+
+    // 활성화 - play
+    $btnPlay.on('click',function(evt){
+        evt.preventDefault();
+        autoPlay();
+    });
+
+    // 활성화 - pause
+    $btnPause.on('click',function(evt){
+        evt.preventDefault();
+        autoPause();
+    });
+
+    // 활성화 - 자동실행 
+    $(window).on('load',function(){
+        autoPlay();
+    });
+
 });
